acbd3970f7357fe4e4dd288fba312c5a62619f38
Jan-Hendrik Sommer <jan-hendrik.sommer@example.com>
2025-11-05 10:35:09 +0100

    Beabsichtigter Fehler

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println("Hello GitHub Actions! 40 + 2 = " + c.add(40,2));
+    }
+}
